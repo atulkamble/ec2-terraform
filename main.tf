e1f8a0e81c43dc6b024e200417734d7f39a18b04
Atul Kamble <atul_kamble@hotmail.com>
2026-01-04 12:12:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.22.1"
+      version = "6.27.0"
     }
   }
 }
@@ -23,33 +23,32 @@ resource "aws_security_group" "mySG" {
   }
 
   ingress {
-    from_port  = 443
-    to_port   = 443
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] 
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port  = 80
-    to_port   = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] 
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-    egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-}
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
 }
 resource "aws_instance" "ec2" {
-  ami           = "ami-0fa3fe0fa7920f68e" # Amazon Linux 2 AMI
-  instance_type = "t3.micro"
+  ami                    = "ami-068c0051b15cdb816" # Amazon Linux 2 AMI
+  instance_type          = "t3.medium"
   vpc_security_group_ids = [aws_security_group.mySG.id]
+  count                  = "1"
 
   tags = {
     Name = "server"
   }
 }
-
-
